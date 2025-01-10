566cacb2be2b3b1a4206bdcad7989699fcfcd3d5
jjoaom <joaomarcos.aquino@hotmail.com>
2025-01-10 11:11:25 -0300

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Bem vindo ao ${siteConfig.title}`}
       description="Documentação Total Gen AI">
       <HomepageHeader />
       <main>
